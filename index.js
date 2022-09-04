8e5877a3274c51b9cad5906492a2c18f257d6239
Wakeel Ogunsanya <wakeel.oguns@gmail.com>
2022-09-04 11:53:28 +0100

    replace var variable keywords with let

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 
-var query = function(data){
+function query(data){
 
+    /**
+     * Generate query string recursively
+     * 
+     * @param {mixed} data 
+     * @param {Array} formatedStr 
+     * @param {String} _base 
+     * 
+     * @returns {String}
+     */
     function generate(data, formatedStr = [], _base) {
-        var dataType = typeof data;
+        let dataType = typeof data;
 
         if(dataType === 'string' || dataType === 'number'){
             return data;
         }
-        for (var key in data) {
-            var val = data[key];
-    
-            var isObject = val &&  typeof val === 'object' ;
+
+        for (let key in data) {
+            let val = data[key];
+            let isObject = val &&  typeof val === 'object' ;
             
             if (isObject) {
-                var k = _base? _base+'['+key+']' : key;
-                generate(val, formatedStr, k);
+                let current = _base? _base+'['+key+']' : key;
+                generate(val, formatedStr, current);
             } else {
-                var _val = encodeURIComponent(val);
-                var query = _base ? _base + '[' + key + ']=' + _val : key + '=' + _val;
+                let _val = encodeURIComponent(val);
+                let query = _base ? _base + '[' + key + ']=' + _val : key + '=' + _val;
                 formatedStr.push(query);
             }
         }
